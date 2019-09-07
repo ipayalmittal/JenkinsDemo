e7b4b8109ca590bacfb9ff6622c6c531226efb49
ipayalmittal <bhartiyapayalb@gmail.com>
2019-09-07 13:49:16 +1000

    test update

--- test/FailedTest.java
+++ test/FailedTest.java
@@ -1,24 +1,18 @@
-import junit.framework.*;
-
-import org.openqa.selenium.By;  
-import org.openqa.selenium.Platform;  
-import org.openqa.selenium.WebDriver;  
-import org.openqa.selenium.WebElement;  
-import org.openqa.selenium.remote.DesiredCapabilities;  
-import org.openqa.selenium.remote.RemoteWebDriver;  
-
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-import java.net.URL;  
-  
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import junit.framework.TestCase;
+
 public class FailedTest extends TestCase {
-  
-  public static void testOne() {  
+
+  public static void testOne() {
     String apiKey = null;
     String apiSecret = null;
 
@@ -32,25 +26,25 @@ public class FailedTest extends TestCase {
          apiKey = data[0];
          apiSecret = data[1];
        } catch (Exception e) { return; }
-      
+
       if ((apiKey == null) || (apiSecret == null)) {
           return;
       }
 
-    DesiredCapabilities caps = new DesiredCapabilities();  
-    caps.setCapability("browserName", "IE");  
-    caps.setCapability("version", "9");  
-    caps.setCapability("platform", "WINDOWS");  
-  
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability("browserName", "IE");
+    caps.setCapability("version", "9");
+    caps.setCapability("platform", "WINDOWS");
+
     try {
-      WebDriver driver = new RemoteWebDriver(new URL("http://" + apiKey + ":" + apiSecret + "@hub.testingbot.com/wd/hub"), caps);  
-      driver.get("https://testingbot.com");  
-      System.out.println("TestingBotSessionID=" + ((RemoteWebDriver) driver).getSessionId().toString());  
-      assertEquals(true, true);
+      WebDriver driver = new RemoteWebDriver(new URL("http://" + apiKey + ":" + apiSecret + "@hub.testingbot.com/wd/hub"), caps);
+      driver.get("https://testingbot.com");
+      System.out.println("TestingBotSessionID=" + ((RemoteWebDriver) driver).getSessionId().toString());
+      assertEquals(false, true);
       driver.quit();
     }
     catch (Exception ex) {
         System.out.println(ex.getMessage());
     }
-  }  
-} 
+  }
+}
