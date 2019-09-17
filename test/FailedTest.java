b64e219ae78fe0ef23507541b4bf915234c68f9d
ipayalmittal <bhartiyapayalb@gmail.com>
2019-09-17 19:05:20 +1000

    compiliation fixed

--- test/FailedTest.java
+++ test/FailedTest.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
@@ -12,7 +13,7 @@ import junit.framework.TestCase;
 
 public class FailedTest extends TestCase {
 
-  public static void testOne() {
+  public static void testOne() throws MalformedURLException {
     String apiKey = null;
     String apiSecret = null;
 
@@ -33,18 +34,13 @@ public class FailedTest extends TestCase {
 
     DesiredCapabilities caps = new DesiredCapabilities();
     caps.setCapability("browserName", "IE");
-    caps.setCapability("version", "9")
+    caps.setCapability("version", "9");
     caps.setCapability("platform", "WINDOWS");
 
-    try {
       WebDriver driver = new RemoteWebDriver(new URL("http://" + apiKey + ":" + apiSecret + "@hub.testingbot.com/wd/hub"), caps);
       driver.get("https://testingbot.com");
       System.out.println("TestingBotSessionID=" + ((RemoteWebDriver) driver).getSessionId().toString());
       assertEquals(true, false);
       driver.quit();
-    }
-    catch (Exception ex) {
-        System.out.println(ex.getMessage());
-    }
   }
 }
--- test/SmokeTest.java
+++ test/SmokeTest.java
@@ -1,24 +1,20 @@
-import junit.framework.*;
-
-import org.openqa.selenium.By;  
-import org.openqa.selenium.Platform;  
-import org.openqa.selenium.WebDriver;  
-import org.openqa.selenium.WebElement;  
-import org.openqa.selenium.remote.DesiredCapabilities;  
-import org.openqa.selenium.remote.RemoteWebDriver;  
-
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-import java.net.URL;  
-  
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import junit.framework.TestCase;
+
 public class SmokeTest  extends TestCase {
-  
-  public static void testOne() {  
+
+  public static void testOne() {
     String apiKey = null;
     String apiSecret = null;
 
@@ -32,29 +28,29 @@ public class SmokeTest  extends TestCase {
          apiKey = data[0];
          apiSecret = data[1];
        } catch (Exception e) { return; }
-      
+
       if ((apiKey == null) || (apiSecret == null)) {
           return;
       }
 
-    DesiredCapabilities caps = new DesiredCapabilities();  
-    caps.setCapability("browserName", "IE");  
-    caps.setCapability("version", "9");  
-    caps.setCapability("platform", "WINDOWS");  
-  
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability("browserName", "IE");
+    caps.setCapability("version", "9");
+    caps.setCapability("platform", "WINDOWS");
+
     try {
-      WebDriver driver = new RemoteWebDriver(new URL("http://" + apiKey + ":" + apiSecret + "@hub.testingbot.com/wd/hub"), caps);  
-      driver.get("http://www.google.com/ncr");  
-      WebElement element = driver.findElement(By.name("q"));  
-    
-      element.sendKeys("TestingBot");  
-      element.submit();  
-    
-      System.out.println("TestingBotSessionID=" + ((RemoteWebDriver) driver).getSessionId().toString());  
+      WebDriver driver = new RemoteWebDriver(new URL("http://" + apiKey + ":" + apiSecret + "@hub.testingbot.com/wd/hub"), caps);
+      driver.get("http://www.google.com/ncr");
+      WebElement element = driver.findElement(By.name("q"));
+
+      element.sendKeys("TestingBot");
+      element.submit();
+
+      System.out.println("TestingBotSessionID=" + ((RemoteWebDriver) driver).getSessionId().toString());
       driver.quit();
     }
     catch (Exception ex) {
         System.out.println(ex.getMessage());
     }
-  }  
-} 
+  }
+}
